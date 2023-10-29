b5c072f2e1e8690ab549209d5df677fe90a3855a
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2023-10-30 04:58:02 +1000

    Паттерн поведения Strategy

---
+++ EventPatterns/Strategy/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+		Car auto = new Car(4, "Volvo", new PetrolMove());
+		auto.Move();
+		auto.Movable = new ElectricMove();
+		auto.Move();
+
+		Console.ReadLine();
+	}
+}
+
+
+
