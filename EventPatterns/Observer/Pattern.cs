b709deeb8e3b1a1124392c419fca46c8a531da6d
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2023-10-30 04:50:45 +1000

    Паттерн поведения Observer

---
+++ EventPatterns/Observer/Pattern.cs
@@ -0,0 +1,119 @@
+﻿namespace App1;
+
+interface IObserver
+{
+	void Update(Object ob);
+}
+
+class StockInfo
+{
+	public int USD { get; set; }
+	public int Euro { get; set; }
+
+}
+
+class Bank : IObserver
+{
+	public string Name { get; set; }
+
+	IObserbvable stock;
+
+	public Bank(string name, IObserbvable obs)
+	{
+		this.Name = name;
+		stock = obs;
+		stock.RegisterObserver(this);
+	}
+
+	public void Update(object ob)
+	{
+		StockInfo sInfo = (StockInfo)ob;
+
+		if (sInfo.USD > 40)
+			Console.WriteLine("Банк {0} продает доллары; Курс {1}", this.Name, sInfo.USD);
+		else
+			Console.WriteLine("Банк {0} покупает доллары; Курс {1}", this.Name, sInfo.USD);
+	}
+
+	public void StopTrade()
+	{
+		stock.RemoveObserver(this);
+		stock = null;
+	}
+}
+
+class Broker : IObserver
+{
+	public string Name { get; set; }
+
+	IObserbvable stock;
+
+	public Broker(string name, IObserbvable obs)
+	{
+		this.Name = name;
+		stock = obs;
+		stock.RegisterObserver(this);
+	}
+
+	public void Update(object ob)
+	{
+		StockInfo sInfo = (StockInfo)ob;
+
+		if (sInfo.USD > 30)
+			Console.WriteLine("Брокер {0} продает доллары; Курс {1}", this.Name, sInfo.USD);
+		else
+			Console.WriteLine("Брокер {0} покупает доллары; Курс {1}", this.Name, sInfo.USD);
+	}
+
+	public void StopTrade()
+	{
+		stock.RemoveObserver(this);
+		stock = null;
+	}
+}
+
+interface IObserbvable
+{
+  void RegisterObserver(IObserver o);
+  void RemoveObserver(IObserver o);
+  void NotifyObserver();
+
+}
+
+class Stock: IObserbvable
+{
+    StockInfo sInfo;
+    List<IObserver> observers;
+
+    public Stock()
+    {
+        observers = new List<IObserver>();
+        sInfo = new StockInfo();
+
+    }
+
+    public void RegisterObserver(IObserver o)
+    {
+        observers.Add(o);
+    }
+
+    public void RemoveObserver(IObserver o)
+    {
+        observers.Remove(o);
+    }
+
+    public void NotifyObserver()
+    {
+        foreach (IObserver observer in observers)
+            observer.Update(sInfo);
+    }
+
+    public void Market()
+    {
+        Random rnd = new Random();
+        sInfo.USD = rnd.Next(20,40);
+        sInfo.Euro = rnd.Next(30, 50);
+        NotifyObserver();
+    }
+
+}
---
+++ EventPatterns/Observer/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace App1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stock stock = new Stock();
+
+        Bank bank = new Bank("Уралсиб", stock);
+        Broker broker = new Broker("Иван Иваныч", stock);
+
+        Broker broker1 = new Broker("Семен Семеныч", stock);
+
+        stock.Market();
+        broker.StopTrade();
+
+        stock.Market();
+
+
+        stock.Market();
+        broker1.StopTrade();
+
+        Console.Read();
+
+    }
+
+}
+
+
+
+
