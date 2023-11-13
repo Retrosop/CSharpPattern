3dd46ebcf589fb1177318028d0fbf6679c55f522
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2023-11-13 13:00:40 +1000

    Add remake pattern Command

---
+++ EventPatterns/Command/Command3add/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+		TV tv = new TV();
+		Volume volume = new Volume();
+		MultiPult mPult = new MultiPult();
+		mPult.SetCommand(0, new TVOnCommand(tv));
+		mPult.SetCommand(1, new VolumeCommand(volume));
+		// включаем телевизор
+		mPult.PressButton(0);
+		// увеличиваем громкость
+		mPult.PressButton(1);
+		mPult.PressButton(1);
+		mPult.PressButton(1);
+		// действия отмены
+		mPult.PressUndoButton();
+		mPult.PressUndoButton();
+		mPult.PressUndoButton();
+		mPult.PressUndoButton();
+
+		Console.Read();
+	}
+}
